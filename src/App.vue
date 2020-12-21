95f424536c89e0cdf71b7a68939b86a5c4205e24
banykim <banykim@gmail.com>
2020-12-21 13:18:21 +0900

    firebase 설정 및 연동

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,105 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <site-title :title="site.title"></site-title>
+
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer">
+      <site-menu :menu="site.menu"></site-menu>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <site-footer :footer="site.footer"></site-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SiteTitle from '@/views/site/Title'
+import SiteFooter from '@/views/site/Footer'
+import SiteMenu from '@/views/site/Menu'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  components: { SiteTitle, SiteFooter, SiteMenu },
+  name: 'App',
+  data: () => ({
+    drawer: false,
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    site: {
+      menu: [
+        {
+          icon: 'mdi-home',
+          title: 'home',
+          subItems: [
+            { title: 'Breakfast & brunch', to: '/' },
+            { title: 'New American' },
+            { title: 'Sushi' }
+          ],
+          to: '/'
+        },
+        {
+          title: 'Dining',
+          icon: 'mdi-silverware-fork-knife',
+          active: true,
+          subItems: [
+            { title: 'Breakfast & brunch' },
+            { title: 'New American' },
+            { title: 'Sushi' }
+          ]
+        },
+        {
+          icon: 'mdi-school',
+          subItems: [{ title: 'List Item' }],
+          title: 'Education'
+        },
+        {
+          icon: 'mdi-run',
+          subItems: [{ title: 'List Item' }],
+          title: 'Family'
+        },
+        {
+          icon: 'mdi-bottle-tonic-plus',
+          subItems: [{ title: 'List Item' }],
+          title: 'Health'
+        },
+        {
+          icon: 'mdi-content-cut',
+          subItems: [{ title: 'List Item' }],
+          title: 'Office'
+        },
+        {
+          icon: 'mdi-tag',
+          subItems: [{ title: 'List Item' }],
+          title: 'Promotions'
+        }
+      ],
+      title: '타이틀영역',
+      footer: '하단영역'
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+    }
+  }),
+  created () {
+    this.subscribe()
+  },
+  mounted () {
+    console.log(this.$firebase)
+  },
+  methods: {
+    subscribe () {
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
+        const v = sn.val()
+        console.log(v)
+        if (!v) {
+          this.$firebase.database().ref().child('site').set(this.site)
+          console.log(this.site)
+        }
+      }, (e) => {
+        console.log(e.message)
+      })
+    }
+  }
+
 }
-</style>
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Vuetify, { VSnackbar, VBtn, VIcon } from 'vuetify/lib'
+import VuetifyToast from 'vuetify-toast-snackbar'
+
+Vue.use(Vuetify, {
+  components: {
+    VSnackbar,
+    VBtn,
+    VIcon
+  }
+})
+
+const opts = {} // your options
+
+const vueObj = new Vuetify(opts)
+
+export default vueObj
+
+Vue.use(VuetifyToast, {
+  $vuetify: vueObj.framework,
+  x: 'right', // default
+  y: 'top', // default
+  color: 'info', // default
+  icon: 'mdi-information-outline',
+  iconColor: '', // default
+  classes: ['body-2'],
+  timeout: 3000, // default
+  dismissable: true, // default
+  multiLine: false, // default
+  vertical: false, // default
+  queueable: false, // default
+  showClose: false, // default
+  closeText: '', // default
+  closeIcon: 'close', // default
+  closeColor: '', // default
+  slot: [], // default
+  shorts: {
+    custom: {
+      color: 'purple'
+    }
+  },
+  property: '$toast' // default
+})
